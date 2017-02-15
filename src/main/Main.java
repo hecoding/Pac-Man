cad0465d4637133320fa422d6cb9718d00bd5b98
hecoding <hlaria@ucm.es>
2017-02-15 20:56:59 +0100

    código para probar árboles y gramática nueva

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,30 @@ import view.gui.swing.GUIView;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// la GUI
 	  	@SuppressWarnings("unused")
 		//CLIView view = new CLIView();
 		GUIView view = new GUIView();
+		
+		
+		/*
+		// para probar árboles
+		ProgramTree tree = TreeParser.toTree("if( distancia >= 1 ) { if( distancia >= 2 ) { comer } } else{ if( distancia >= 3 ) { comer } }");
+		System.out.println(tree);
+		*/
+		
+		/*
+		// para probar generación de gramáticas
+		int NUM_DE_DERIVACIONES_A_GENERAR = 20;
+		PacmanGrammaticalEvolution problem = new PacmanGrammaticalEvolution("test/gramaticadelaqueseforjanlossuenos.bnf",
+				100, 100, 0.02, 0.6, null, 3, 2, 100, 3, 256
+				);
+		Solutions<Variable<Integer>> solutions = problem.newRandomSetOfSolutions(NUM_DE_DERIVACIONES_A_GENERAR);
+		for(Solution<Variable<Integer>> sol : solutions) {
+			System.out.println(problem.generatePhenotype(sol));
+		}
+		*/
 	}
 
 }
