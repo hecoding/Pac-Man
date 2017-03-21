c5b7193b1b0ce3f193a4c0c3ae08a096d1d5a014
Jorge Sanchez <jorjatorz@gmail.com>
2017-03-21 21:36:34 +0100

    Created new LHSCrossover class

---
+++ src/jeco/core/operator/crossover/LHSCrossover.java
@@ -0,0 +1,16 @@
+package jeco.core.operator.crossover;
+
+import jeco.core.problem.Solution;
+import jeco.core.problem.Solutions;
+import jeco.core.problem.Variable;
+
+public class LHSCrossover<T extends Variable<?>> extends CrossoverOperator<T>
+{
+
+	@Override
+	public Solutions<T> execute(Solution<T> parent1, Solution<T> parent2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
