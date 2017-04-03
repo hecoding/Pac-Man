24770b30a24d4ebb869b907f9ac2115239ae287b
J-888 <josemigueltajuelo@gmail.com>
2017-04-03 22:55:30 +0200

    Implemented LevelsCompleted fitness

---
+++ src/jeco/core/operator/evaluator/fitness/LevelsCompletedFitness.java
@@ -0,0 +1,26 @@
+package jeco.core.operator.evaluator.fitness;
+
+import pacman.game.util.GameInfo;
+
+public class LevelsCompletedFitness implements FitnessEvaluatorInterface {
+
+	@Override
+	public double evaluate(GameInfo gi) {
+		return 100 - gi.getAvgLastLevelReached();
+	}
+	
+	public static double fitnessToPoints(double fitness) {
+		return 100 - fitness;
+	}
+	
+	@Override
+	public double worstFitness() {
+		return 100;
+	}
+
+	@Override
+	public String getName() {
+		return "Points w/o ghost mult Fitness";
+	}
+
+}
