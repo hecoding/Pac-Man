ccffc149e126ee31adf795ed0e9113940de02554
hecoding <hlaria@ucm.es>
2017-05-08 04:40:03 +0200

    Add cppn structure

--- src/jeco/core/algorithm/neat/CPPN.java
+++ src/jeco/core/algorithm/neat/CPPN.java
@@ -1,4 +1,22 @@
 package jeco.core.algorithm.neat;
 
+import java.util.ArrayList;
+
 public class CPPN {
+    ArrayList<Node> nodes;
+    ArrayList<Link> links;
+
+    class Node {
+        NodeType type;
+    }
+
+    class Link {
+        Node in;
+        Node out;
+        double weight;
+        boolean enabled;
+        int innov;
+    }
+
+    public enum NodeType {NEURON, INPUT}
 }
