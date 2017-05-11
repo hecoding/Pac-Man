718462ffd72d36baeb25e99067daa8370811104b
hecoding <hlaria@ucm.es>
2017-05-11 02:59:37 +0200

    Add all selection operators

--- src/view/gui/swing/factory/SelectionOperatorFactory.java
+++ src/view/gui/swing/factory/SelectionOperatorFactory.java
@@ -1,8 +1,8 @@
 package view.gui.swing.factory;
 
 import jeco.core.algorithm.moge.PacmanGrammaticalEvolution;
-import jeco.core.operator.selection.BinaryTournamentNSGAII;
-import jeco.core.operator.selection.SelectionOperator;
+import jeco.core.operator.comparator.SimpleDominance;
+import jeco.core.operator.selection.*;
 import jeco.core.problem.Variable;
 
 public class SelectionOperatorFactory {
@@ -18,15 +18,23 @@ public class SelectionOperatorFactory {
     }
 
     public String[] getRegisteredKeys() {
-        String[] a = {"BinaryTournamentNSGAII"}; //TODO add all
+        String[] a = {"BinaryTournament", "BinaryTournamentNSGAII", "TournamentSelect", "EliteSelectorOperator"};
         return a;
     }
 
     public SelectionOperator create(String id, PacmanGrammaticalEvolution problem) {
 
-        if (id.equals("BinaryTournamentNSGAII"))
-            return new BinaryTournamentNSGAII<Variable<Integer>>();
-
-        throw new InstantiationError("Unspecified instantiation of class: " + id);
+        switch (id) {
+            case "BinaryTournament":
+                return new BinaryTournament<Variable<Integer>>(new SimpleDominance<>());
+            case "BinaryTournamentNSGAII":
+                return new BinaryTournamentNSGAII<Variable<Integer>>();
+            case "TournamentSelect":
+                return new TournamentSelect<Variable<Integer>>(new SimpleDominance<>());
+            case "EliteSelectorOperator":
+                return new EliteSelectorOperator<Variable<Integer>>(10);
+            default:
+                throw new InstantiationError("Unspecified instantiation of class: " + id);
+        }
     }
 }
