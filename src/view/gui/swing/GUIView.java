aace7610b2e9ec63b82491da61b3cffbeb1f01f8
J-888 <josemigueltajuelo@gmail.com>
2017-06-16 09:41:48 +0200

    final changes

--- src/view/gui/swing/GUIView.java
+++ src/view/gui/swing/GUIView.java
@@ -58,7 +58,7 @@ public class GUIView extends JFrame {
 		this.add(mainPanel);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(new Dimension(1268,827));
+		this.setSize(new Dimension(1268,845));
 		this.setMinimumSize(new Dimension(750, 550));
 		this.setLocationRelativeTo(null); // center on the screen (doesn't show nice with multiple monitors)
 		this.setVisible(true);
